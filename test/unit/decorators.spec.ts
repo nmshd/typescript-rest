f7df90fa7c629ae37d9c24bde3fb03bff5678e26
Julian König <julian.koenig@js-soft.com>
2025-08-15 14:46:25 +0200

    test: update more tests

--- test/unit/decorators.spec.ts
+++ test/unit/decorators.spec.ts
@@ -127,7 +127,7 @@ describe('Decorators', () => {
         });
 
         it('should add a security set of roles to methods of a class', () => {
-            serviceDecorators.Security(['test-role', 'tes-role2'])(
+            serviceDecorators.Security(['test-role', 'test-role2'])(
                 TestService.prototype,
                 'test',
                 Object.getOwnPropertyDescriptor(TestService.prototype, 'test')
@@ -137,19 +137,6 @@ describe('Decorators', () => {
             expect(serviceMethod.authenticator.default).toHaveLength(2);
             expect(serviceMethod.authenticator.default).toContain('test-role');
             expect(serviceMethod.authenticator.default).toContain('test-role2');
-        });
-
-        it('should add a security validation to accept any role when undefined is received', () => {
-            const role: string = undefined;
-            serviceDecorators.Security(role)(
-                TestService.prototype,
-                'test',
-                Object.getOwnPropertyDescriptor(TestService.prototype, 'test')
-            );
-
-            expect(serverContainer.registerServiceMethod).toHaveBeenCalledTimes(1);
-            expect(serviceMethod.authenticator.default).toHaveLength(1);
-            expect(serviceMethod.authenticator.default).toContain('*');
         });
 
         it('should set the default authenticator if no name is provided', () => {
